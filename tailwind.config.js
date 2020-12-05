787dd822bd3ba3f4d223c6f9e56f633ce1f4866c
tMinamiii <otherthan.new@gmail.com>
2020-12-05 17:34:28 +0900

    入れ子リストに記号をつける

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,9 @@
+//https://tailwindcss.com/docs/list-style-type
 module.exports = {
   purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
-    // listStyleType: {
-    //   none: 'none',
-    //   disc: 'disc',
-    //   decimal: 'decimal',
-    //   square: 'square',
-    //   roman: 'upper-roman',
-    // },
   },
   variants: {
     extend: {},
