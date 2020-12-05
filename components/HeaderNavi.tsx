00673f0be342a8d815930b301518dc3fb43962a7
tMinamiii <otherthan.new@gmail.com>
2020-12-05 18:30:08 +0900

    ヘッダーの文字サイズを下げる

--- components/HeaderNavi.tsx
+++ components/HeaderNavi.tsx
@@ -4,15 +4,15 @@ import React, { ReactElement } from 'react';
 const HeaderNavi: React.FC = (): ReactElement => {
   return (
     <header>
-      <nav className="text-center text-2xl p-4">
+      <nav className="text-center text-xl p-4">
         <Link href="/">
           <a className="underline">Home</a>
         </Link>
-        {' / '}
+        {' - '}
         <Link href="/resume">
           <a className="underline">Resume</a>
         </Link>
-        {' / '}
+        {' - '}
         <Link href="/articles">
           <a className="underline">Articles</a>
         </Link>{' '}
