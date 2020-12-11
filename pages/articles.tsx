e19eea09e7dcc5a154fdc7b50127caf214af125a
tMinamiii <otherthan.new@gmail.com>
2020-12-11 21:48:08 +0900

    articleページもborderでかこう

--- pages/articles.tsx
+++ pages/articles.tsx
@@ -6,7 +6,13 @@ const ArticlesPage: React.FC = (): ReactElement => {
     <div>
       <HeaderElements title="Articles" />
       <HeaderNavi />
-      Nothing..
+      <div className="grid sm:grid-cols-10 md:grid-cols-12">
+        <div className="sm:col-span-1 md:col-span-1" />
+        <div className="markdown-body rounded-lg border-solid border-2 sm:col-span-8 md:col-span-10 p-10">
+          Nothing..
+        </div>
+        <div className="sm:col-span-1 md:col-span-1" />
+      </div>
     </div>
   );
 };
