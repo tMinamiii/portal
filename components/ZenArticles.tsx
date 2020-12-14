6ce32c5dabc74a9bb1c2b66f75ee82d87aba46f9
tMinamiii <otherthan.new@gmail.com>
2020-12-15 02:48:15 +0900

    zennのRSSから記事一覧ページを作成する

--- components/ZenArticles.tsx
+++ components/ZenArticles.tsx
@@ -1,46 +1,18 @@
 import React, { ReactElement } from 'react';
-import ArticleLinkList from '../components/ArticleLinkList';
 import Border from '../components/Border';
 
-const articles = [
-  {
-    title: 'Cookpadの検索システムに痺れたので語りたい',
-    url: 'https://zenn.dev/tminamiii/articles/80118de1b400df16c3e3',
-  },
-  {
-    title: 'OpenAPIのYAMLをVSCodeで分割管理する',
-    url: 'https://zenn.dev/tminamiii/articles/97e9aab5d44af5',
-  },
-  {
-    title: 'GistのMarkdownファイルをNext.jsで読み込んで表示する',
-    url: 'https://zenn.dev/tminamiii/articles/e70589dbf19b9a',
-  },
-  {
-    title: 'Gnome Terminalのフォントを変更する',
-    url: 'https://zenn.dev/tminamiii/articles/b4302125c89fdc',
-  },
-  {
-    title: 'S3のイベントをHookしてLambdaを実行する(Go言語)',
-    url: 'https://zenn.dev/tminamiii/articles/b26a773cef9ff2',
-  },
-  {
-    title: 'Next.js + tailwindcss + ReactMarkdownで経歴書作成',
-    url: 'https://zenn.dev/tminamiii/articles/f3cec628f70109',
-  },
-];
+type Props = {
+  articles: Array<ReactElement>;
+};
 
-const ZenArticles: React.FC = (): ReactElement => {
-  const articleList = [];
-  for (const art of articles) {
-    articleList.push(<ArticleLinkList title={art.title} url={art.url} />);
-  }
+const ZenArticles: React.FC<Props> = ({articles}: Props): ReactElement => {
   return (
     <Border
       element={
         <div>
           <div className="text-xl font-bold">Zen</div>
           <div className="list-mark">
-            <ul>{articleList}</ul>
+            <ul>{articles}</ul>
           </div>
         </div>
       }
--- pages/articles.tsx
+++ pages/articles.tsx
@@ -1,14 +1,36 @@
 import React, { ReactElement } from 'react';
+import Parser from 'rss-parser';
+import ArticleLinkList from '../components/ArticleLinkList';
 import HeaderElements from '../components/HeaderElements';
 import HeaderNavi from '../components/HeaderNavi';
 import ZenArticles from '../components/ZenArticles';
 
-const ArticlesPage: React.FC = (): ReactElement => {
+type Props = {
+  zenn: any;
+};
+
+export async function getServerSideProps(): Promise<any> {
+  let parser = new Parser()
+  const zennRssUrl = 'https://zenn.dev/tminamiii/feed';
+  const feed = await parser.parseURL(zennRssUrl);
+  return { props: { zenn: JSON.stringify(feed) } };
+}
+
+
+const ArticlesPage: React.FC<Props> = ({zenn}: Props): ReactElement => {
+  const feed = JSON.parse(zenn)
+  const articles: Array<ReactElement> = [];
+  feed.items.map((f: any, i: number) => {
+    if (f.title && f.link) {
+      articles.push(<ArticleLinkList key={`zenn_${i}`} title={f.title} url={f.link} />);
+    }
+  })
+
   return (
     <div>
       <HeaderElements title="Articles" />
       <HeaderNavi />
-      <ZenArticles />
+      <ZenArticles articles={articles} />
     </div>
   );
 };
