ba1957885c42553f8a1c2f0dc4a10e3d05cdb8c2
tMinamiii <otherthan.new@gmail.com>
2020-12-15 11:19:14 +0900

    qiiitaの記事もRSSから読み取る

--- pages/articles.tsx
+++ pages/articles.tsx
@@ -7,33 +7,42 @@ import HeaderNavi from '../components/HeaderNavi';
 
 type Props = {
   zennFeed: string;
+  qiitaFeed: string;
 };
 
 export async function getServerSideProps(): Promise<any> {
   let parser = new Parser()
-  const zennRssUrl = 'https://zenn.dev/tminamiii/feed';
-  const feed = await parser.parseURL(zennRssUrl);
-  return { props: { zennFeed: JSON.stringify(feed) } };
+  const zennFeed = await parser.parseURL('https://zenn.dev/tminamiii/feed');
+  const qiitaFeed = await parser.parseURL('https://qiita.com/tMinamiii/feed.atom');
+
+  return { props: { zennFeed: JSON.stringify(zennFeed), qiitaFeed: JSON.stringify(qiitaFeed) } };
 }
 
-function fetchZenArticles(zennFeed: string): Array<ReactElement> {
-  const feed = JSON.parse(zennFeed)
+function fetchZenArticles(feedStr: string): Array<ReactElement> {
+  const ignores = [
+  "https://zenn.dev/tminamiii/articles/97e9aab5d44af5",
+  "https://zenn.dev/tminamiii/articles/b4302125c89fdc",
+    "https://zenn.dev/tminamiii/articles/b26a773cef9ff2",
+  ]
+  const feed = JSON.parse(feedStr)
   const articles: Array<ReactElement> = [];
   feed.items.map((f: any, i: number) => {
-    if (f.title && f.link) {
+    if (f.title && f.link && !ignores.includes(f.link)) {
       articles.push(<ArticleLinkList key={`zenn_${i}`} title={f.title} url={f.link} />);
     }
   })
   return articles;
 }
 
-const ArticlesPage: React.FC<Props> = ({zennFeed}: Props): ReactElement => {
-  const articles = fetchZenArticles(zennFeed)
+const ArticlesPage: React.FC<Props> = ({zennFeed, qiitaFeed}: Props): ReactElement => {
+  const zenn = fetchZenArticles(zennFeed)
+  const qiita = fetchZenArticles(qiitaFeed)
   return (
     <div>
       <HeaderElements title="Articles" />
       <HeaderNavi />
-      <ArticlesArea title="Zenn" articles={articles} />
+      <ArticlesArea title="Zenn" articles={zenn} />
+      <ArticlesArea title="Qiita" articles={qiita} />
     </div>
   );
 };
