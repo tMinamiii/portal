90ee5322d71595985e9d75c61f8016ebe730ad9f
takahiro-minami <takahiro.minami@every.tv>
2021-07-27 13:23:37 +0900

    rename

--- components/OtherArticles.tsx
+++ components/OtherArticles.tsx
@@ -7,10 +7,10 @@ type Props = {
   feed: string;
 };
 const media = 'Other';
-const QiitaArticles: React.FC<Props> = (): ReactElement => {
+const OtherArticles: React.FC<Props> = (): ReactElement => {
   const articles: Array<ReactElement> = [];
   articles.push(<ArticleLinks key="every_1" title="DELISH KITCHENチラシの郵便番号・地域名・店舗名検索実装について" url="https://tech.every.tv/entry/2021/07/27/120251" />);
   const articleList = <ArticleList media={media} articles={articles} />;
   return <Border element={articleList} />;
 };
-export default QiitaArticles;
+export default OtherArticles;
