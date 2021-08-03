cf683c2e1f11f4e160797ac7c7197d6a40a4427a
takahiro-minami <takahiro.minami@every.tv>
2021-08-03 11:44:15 +0900

    add Note articles

---
+++ components/NoteArticles.tsx
@@ -0,0 +1,21 @@
+import React, { ReactElement } from 'react';
+import ArticleLinks from './ArticleLinks';
+import ArticleList from './ArticleList';
+import Border from './Border';
+
+type Props = {
+  feed: string;
+};
+const media = 'Note';
+const QiitaArticles: React.FC<Props> = ({ feed }: Props): ReactElement => {
+  const feedobj = JSON.parse(feed);
+  const articles: Array<ReactElement> = [];
+  feedobj.items.map((f: any, i: number) => {
+    if (f.title && f.link) {
+      articles.push(<ArticleLinks key={`${media}_${i}`} title={f.title} url={f.link} />);
+    }
+  });
+  const articleList = <ArticleList media={media} articles={articles} />;
+  return <Border element={articleList} />;
+};
+export default QiitaArticles;
--- pages/articles.tsx
+++ pages/articles.tsx
@@ -3,6 +3,7 @@ import Parser from 'rss-parser';
 import HeaderElements from '../components/HeaderElements';
 import HeaderNavi from '../components/HeaderNavi';
 import QiitaArticles from '../components/QiitaArticles';
+import NoteArticles from '../components/NoteArticles';
 import OtherArticles from '../components/OtherArticles';
 import ScrapBoxArticles from '../components/ScrapBoxArticles';
 import ZennArticles from '../components/ZennArticles';
@@ -11,6 +12,7 @@ type Props = {
   zennFeed: string;
   qiitaFeed: string;
   scrapBoxFeed: string;
+  noteFeed: string;
 };
 
 export async function getStaticProps(): Promise<any> {
@@ -18,20 +20,24 @@ export async function getStaticProps(): Promise<any> {
   const zennFeedP = parser.parseURL('https://zenn.dev/tminamiii/feed');
   const qiitaFeedP = parser.parseURL('https://qiita.com/tMinamiii/feed.atom');
   const scrapBoxFeedP = parser.parseURL('https://scrapbox.io/api/feed/tMinamiii');
+  const noteFeedP = parser.parseURL('https://note.com/tminami/rss');
   const zennFeed = await zennFeedP;
   const qiitaFeed = await qiitaFeedP;
   const scrapBoxFeed = await scrapBoxFeedP;
+  const noteFeed = await noteFeedP;
+
   return {
     props: {
       zennFeed: JSON.stringify(zennFeed),
       qiitaFeed: JSON.stringify(qiitaFeed),
       scrapBoxFeed: JSON.stringify(scrapBoxFeed),
+      noteFeed: JSON.stringify(noteFeed),
     },
     revalidate: 300,
   };
 }
 
-const ArticlesPage: React.FC<Props> = ({ zennFeed, qiitaFeed, scrapBoxFeed }: Props): ReactElement => {
+const ArticlesPage: React.FC<Props> = ({ zennFeed, qiitaFeed, scrapBoxFeed, noteFeed }: Props): ReactElement => {
   return (
     <div>
       <HeaderElements title="Articles" />
@@ -39,6 +45,7 @@ const ArticlesPage: React.FC<Props> = ({ zennFeed, qiitaFeed, scrapBoxFeed }: Pr
       <ZennArticles feed={zennFeed} />
       <QiitaArticles feed={qiitaFeed} />
       <ScrapBoxArticles feed={scrapBoxFeed} />
+      <NoteArticles feed={noteFeed} />
       <OtherArticles />
     </div>
   );
