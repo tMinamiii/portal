648829894afecff94643a64c4110250dbf84e41b
tMinamiii <otherthan.new@gmail.com>
2021-10-10 11:05:49 +0900

    feat: remove scrapbox articles

--- pages/articles.tsx
+++ pages/articles.tsx
@@ -2,10 +2,9 @@ import React, { ReactElement } from 'react';
 import Parser from 'rss-parser';
 import HeaderElements from '../components/HeaderElements';
 import HeaderNavi from '../components/HeaderNavi';
-import QiitaArticles from '../components/QiitaArticles';
 import NoteArticles from '../components/NoteArticles';
 import OtherArticles from '../components/OtherArticles';
-import ScrapBoxArticles from '../components/ScrapBoxArticles';
+import QiitaArticles from '../components/QiitaArticles';
 import ZennArticles from '../components/ZennArticles';
 
 type Props = {
@@ -19,32 +18,31 @@ export async function getStaticProps(): Promise<any> {
   const parser = new Parser();
   const zennFeedP = parser.parseURL('https://zenn.dev/tminamiii/feed');
   const qiitaFeedP = parser.parseURL('https://qiita.com/tMinamiii/feed.atom');
-  const scrapBoxFeedP = parser.parseURL('https://scrapbox.io/api/feed/tMinamiii');
+  //const scrapBoxFeedP = parser.parseURL('https://scrapbox.io/api/feed/tMinamiii');
   const noteFeedP = parser.parseURL('https://note.com/tminami/rss');
   const zennFeed = await zennFeedP;
   const qiitaFeed = await qiitaFeedP;
-  const scrapBoxFeed = await scrapBoxFeedP;
+  // const scrapBoxFeed = await scrapBoxFeedP;
   const noteFeed = await noteFeedP;
 
   return {
     props: {
       zennFeed: JSON.stringify(zennFeed),
       qiitaFeed: JSON.stringify(qiitaFeed),
-      scrapBoxFeed: JSON.stringify(scrapBoxFeed),
+      // scrapBoxFeed: JSON.stringify(scrapBoxFeed),
       noteFeed: JSON.stringify(noteFeed),
     },
     revalidate: 300,
   };
 }
 
-const ArticlesPage: React.FC<Props> = ({ zennFeed, qiitaFeed, scrapBoxFeed, noteFeed }: Props): ReactElement => {
+const ArticlesPage: React.FC<Props> = ({ zennFeed, qiitaFeed, noteFeed }: Props): ReactElement => {
   return (
     <div>
       <HeaderElements title="Articles" />
       <HeaderNavi />
       <ZennArticles feed={zennFeed} />
       <QiitaArticles feed={qiitaFeed} />
-      <ScrapBoxArticles feed={scrapBoxFeed} />
       <NoteArticles feed={noteFeed} />
       <OtherArticles />
     </div>
